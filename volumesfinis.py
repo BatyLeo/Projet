a913dd9e698fa6b88b094b18926eddb391c52564
BatyLeo <leo.baty@eleves.enpc.fr>
2018-05-17 08:56:27 +0200

    Plot 3D du code volume fini

--- volumesfinis.py
+++ volumesfinis.py
@@ -1,7 +1,17 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from riemann_scal import *
+
+codim0=np.load('codim0.npy')
+codim1=np.load('codim1.npy')
+codim0to1A=np.load('codim0to1A.npy')
+codim0to1B=np.load('codim0to1B.npy')
+codim0to1E=np.load('codim0to1E.npy')
+codim0to1NX=np.load('codim0to1NX.npy')
+codim0to1NY=np.load('codim0to1NY.npy')
+
 
 x0 = 0; y0 = 0 # On définit l'origine du repère 2D (coordonnées de la première face)
 # Domaine de résolution
@@ -23,22 +33,19 @@ volume = hx*hy # volume d'une cellule
 neighbours = 4 # nombre de voisins par cellule
 
 def ux(i):
-    return 0
+    return 1
     
 def uy(i):
-    return 2
+    return 0
 
-codim0=np.load('codim0.npy')
-codim1=np.load('codim1.npy')
-codim0to1A=np.load('codim0to1A.npy')
-codim0to1B=np.load('codim0to1B.npy')
-codim0to1E=np.load('codim0to1E.npy')
-codim0to1NX=np.load('codim0to1NX.npy')
-codim0to1NY=np.load('codim0to1NY.npy')
-
+## Initialisation
 qini = np.zeros(codim0.shape[0])
 
-qini[0]=1
+qini[10]=1
+qini[0:3]=3*[0.5]
+qini[9]=0.5
+qini[11]=0.5
+qini[18:21]=[0.5,0.5,0.5]
 
 qhist = [qini.copy()]
 
@@ -49,42 +56,51 @@ q1 = qini.copy()
 t = 0
 Tfinal = 10
 
+## Boucle temporelle
 while t<Tfinal:
     k = 1000
-    flux=np.zeros(codim0.shape[0])
+    flux=np.zeros(codim0.shape[0]) # Matrice des flux
+    # On parcours toutes les faces
     for iface in range(codim1.shape[0]):
-        i=codim0to1A[iface]
-        j=codim0to1B[iface]
-        if i<0 or j<0:
+        i=codim0to1A[iface] # Cellule en amont
+        j=codim0to1B[iface] # Cellule en aval
+        if i<0 or j<0: # Conditions de bord périodiques
             continue
-        lambdai = ux(i)*codim0to1NX[iface]+uy(i)*codim0to1NY[iface]
+        # Vitesse du fluide en i et j projeté selon la normale à la face
+        lambdai = ux(i)*codim0to1NX[iface]+uy(i)*codim0to1NY[iface] 
         lambdaj = ux(j)*codim0to1NX[iface]+uy(j)*codim0to1NY[iface]
-        statei = q0[i]
-        statej = q0[j]
-        [leftf,rightf,Lambda] = RIEMANN(lambdai,statei,lambdaj,statej)
-        
+        statei = q0[i] # Concentration en i
+        statej = q0[j] # Concentration en j
+        # Schéma de Lax-Friedriech: calcul du flux en i et en j à travers la face
+        [leftf,rightf,Lambda] = RIEMANN(lambdai,statei,lambdaj,statej) 
+        # Mise à jour des flux
         flux[i]+=leftf*hx
         flux[j]+=rightf*hx
-
+        # Calcul du pas de temps associé
         k=min(k,volume/codim0to1E[iface]/Lambda)
-    
-    #print(k,volume,Lambda)
+    # Mise à jour de la concentration
     q1+=flux*k/volume
-    print(q0[0::9])
-    
     qhist.append(q1.copy())
     q0 = q1.copy()
     t+=k
 
+## Plot 2D
+#for q in qhist:
+#    plt.matshow(q.reshape((Nx+1,Ny+1)),False,origin='lower')
+#    plt.pause(0.001)
 
 
 
+## Plot 3D
+for q in qhist:
+    plt.clf()
+    fig = plt.figure(1)
+    ax = fig.gca(projection='3d')
+    X=np.array([codim0[i,0] for i in range(codim0.shape[0])])
+    Y=np.array([codim0[i,1] for i in range(codim0.shape[0])])
+    x=X.reshape(Nx+1,Ny+1)
+    y=Y.reshape(Nx+1,Ny+1)
+    ax.plot_wireframe(x,y,q.reshape(Nx+1,Ny+1))#,False)
+    plt.pause(0.1)
 
-
-
-
-
-
-
-    
 
